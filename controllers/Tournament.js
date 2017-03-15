87638d7db3ad978cbb2751afc43ad36efeaca0be
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-15 16:01:54 +0000

    Removes unecessary triple dots

--- controllers/Tournament.js
+++ controllers/Tournament.js
@@ -3,7 +3,8 @@ import Tournament from '../models/Tournament';
 export const index = (req, res, next) => {
   Tournament.find().lean().exec((err, tournaments) => res.json(
     { tournaments: tournaments.map(tournament => ({
-      ...tournament
+      tournament
     }))}
   ));
+
 };
